957fc00d225dc5bb010a5fb0a77e9900c823a48f
zatomis <zatomis@list.ru>
2023-06-14 13:18:44 +0300

    add datetime fields to PokemonEntity. add specs to PokemonEntity model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,6 @@
 from django.db import models  # noqa F401
 from datetime import datetime
+import random
 
 
 class PokemonEntity(models.Model):
@@ -14,7 +15,13 @@ class Pokemon(models.Model):
     title = models.TextField()
     photo = models.ImageField(blank=True)
     coordinats = models.ForeignKey(PokemonEntity, on_delete=models.CASCADE)
-    appeared_at = models.DateTimeField(default=datetime.now())
-    disappeared_at = models.DateTimeField(default=datetime.now())
-    def __str__(self):
+    appeared_at = models.DateTimeField(help_text="появится",default=datetime.now())
+    disappeared_at = models.DateTimeField(help_text="пропадет",default=datetime.now())
+    level = models.IntegerField(help_text="уровень", default=random.randint(5,10))
+    health = models.IntegerField(help_text="здоровье", default=random.randint(5,10))
+    attack = models.IntegerField(help_text="атака", default=random.randint(5,10))
+    defense = models.IntegerField(help_text="защита", default=random.randint(5,10))
+    stamina = models.IntegerField(help_text="выносливость", default=random.randint(5,10))
+
+def __str__(self):
         return f'{self.title}'
