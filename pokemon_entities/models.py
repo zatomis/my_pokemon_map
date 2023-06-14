ec240643a3aaf5960136e6caf34e6069b1921d59
zatomis <zatomis@list.ru>
2023-06-14 09:55:28 +0300

    create Pokemon model register Pokemon model on admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,9 @@
 from django.db import models  # noqa F401
 
-# your models here
+
+class Pokemon(models.Model):
+    title = models.TextField()
+    photo = models.ImageField(blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
